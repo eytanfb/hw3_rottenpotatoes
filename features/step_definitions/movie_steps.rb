2d0e669d16b4b1c816b7274fc985281b6b2319e3
Eytan Anjel <eytanfb@gmail.com>
2012-03-16 16:20:32 +0200

    hw3 part 2 a&b completed

--- features/step_definitions/movie_steps.rb
+++ features/step_definitions/movie_steps.rb
@@ -4,8 +4,8 @@ Given /the following movies exist/ do |movies_table|
   movies_table.hashes.each do |movie|
     # each returned element will be a hash whose key is the table header.
     # you should arrange to add that movie to the database here.
+    Movie.create!(movie)
   end
-  assert false, "Unimplmemented"
 end
 
 # Make sure that one string (regexp) occurs before or after another one
@@ -25,4 +25,23 @@ When /I (un)?check the following ratings: (.*)/ do |uncheck, rating_list|
   # HINT: use String#split to split up the rating_list, then
   #   iterate over the ratings and reuse the "When I check..." or
   #   "When I uncheck..." steps in lines 89-95 of web_steps.rb
+  rating_list.split(", ").each do |rating|
+    if uncheck
+      uncheck(rating)
+    else
+      check(rating)
+    end
+  end
 end
+
+And /^I should see$/ do |table|
+  # table is a Cucumber::Ast::Table
+  table.hashes.each do |movie|
+    Movie.find_by_rating('PG') || Movie.find_by_rating('R')
+  end
+end
+
+Then /I should see all of the movies/
+  assert true, rows.should == Movie.all.count
+end
+
